629144f3af401b322bcd5a25afcbb4cddca3716a
clintontong <clintong1018@gmail.com>
2016-07-05 15:48:34 -0700

    removed messages controller and model

--- app/models/message.rb
+++
@@ -1,8 +0,0 @@
-class Message < ActiveRecord::Base
-  belongs_to :sender, class_name: 'User'
-  has_many :recipients
-  has_many :users, :through => :recipients
-
-  validates :sender_id, presence: true
-  validates :body, presence: true
-end
