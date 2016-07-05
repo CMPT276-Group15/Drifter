c107c1c526ac82db710ade0f22f294c2889ece05
jeff <mingjiey@sfu.ca>
2016-07-05 03:45:54 +0000

    Update Information and new Show all Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update] 
   before_action :correct_user, only: [:edit, :update] 
   def index
-    @users = User.all 
+    @users = User.paginate(page: params[:page]) 
   end
   def show
    @user = User.find(params[:id])
@@ -25,10 +25,13 @@ class UsersController < ApplicationController
   
   def edit 
     
+    
   end
   def update 
     @user = User.find(params[:id]) 
-    if @user.update_attributes(user_params) 
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated" 
+      redirect_to @user 
   else
     render 'edit' 
   end
