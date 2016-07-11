2e3e2041728e6054db02568f44e3b526da60b1af
David <dpmii@hotmail.ca>
2016-07-11 02:08:48 -0700

    Rejigger user page Insert Map Box

--- db/schema.rb
+++ db/schema.rb
@@ -48,6 +48,13 @@ ActiveRecord::Schema.define(version: 20160706222245) do
   add_index "journals", ["user_id", "created_at"], name: "index_journals_on_user_id_and_created_at", using: :btree
   add_index "journals", ["user_id"], name: "index_journals_on_user_id", using: :btree
 
+  create_table "messages", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "sender_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
     t.integer  "user_id"
@@ -72,6 +79,10 @@ ActiveRecord::Schema.define(version: 20160706222245) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
+
+  add_index "relationships", ["followed_id"], name: "index_relationships_on_followed_id", using: :btree
+  add_index "relationships", ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true, using: :btree
+  add_index "relationships", ["follower_id"], name: "index_relationships_on_follower_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
