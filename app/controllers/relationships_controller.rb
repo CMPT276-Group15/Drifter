6d6ff7d255d769c06af15214faea642c746b52f7
David <dpmii@hotmail.ca>
2016-07-31 16:20:03 -0700

    Refix relationship controller

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,5 +1,6 @@
 class RelationshipsController < ApplicationController
-  #before_action :logged_in_user  #Commented out because broke?
+  #Recurring Issue,line 3 !!v
+  before_action :logged_in_user, only: [:edit, :update]
   def create 
     user = User.find(params[:followed_id]) 
     current_user.follow(user) 
