7003424094b58db45aaf2e07bc72bac4b7cc0acc
David <dpmii@hotmail.ca>
2016-07-31 16:12:33 -0700

    Refix relationships_controller; Note that this is a recurring error and should be watched for

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,5 +1,5 @@
 class RelationshipsController < ApplicationController
-  before_action :logged_in_user
+  #before_action :logged_in_user  #Commented out because broke?
   def create 
     user = User.find(params[:followed_id]) 
     current_user.follow(user) 
