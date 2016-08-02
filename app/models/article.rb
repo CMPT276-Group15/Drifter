a399e9f90ba11c6d00852b1227e7e3188f695c00
Di Wang <dwa79@sfu.ca>
2016-08-02 01:08:17 +0000

    fix article view

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,7 @@ class Article < ActiveRecord::Base
   belongs_to :user
   has_many :comments, dependent: :destroy
   validates :title, presence: true,
-                    length: { minimum: 5 }
+                    length: { minimum: 5, maximum: 50 }
+  validates :text, presence: true, length:{ minimum: 10, maximum: 2000 }
   validates :user_id, presence: true
 end
